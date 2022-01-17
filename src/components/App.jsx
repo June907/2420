9ae518e85fb63d6d396f733f0c46034e773fed30
June <57102696+June907@users.noreply.github.com>
2022-01-17 14:42:19 -0700

    homePage

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+import HomePage from "./homePage/HomePage";
+function App(){
+  return(
+  <div >
+  
+  <HomePage/>
+  
+    
+
+
+    
+  
+  
+  </div>)
+    
+  
+
+}
+
+export default App;
