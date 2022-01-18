f67bcaf1e5bb9a02726cb276b0c915d2bd16522e
June <57102696+June907@users.noreply.github.com>
2022-01-18 13:55:28 -0700

    deploy page

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -3,19 +3,40 @@ import Stock from "./Stock";
 import Header from "./Header";
 import Footer from "./Footer";
 import Content from "./Content";
+import HomePageFeature from "./HomePageFeature";
+import HomePageIntro from "./HomePageIntro";
+import HomePageReviewIntro from "./HomePageReviewIntro";
+import ReviewContent from "./ReviewContent";
 
 export default function HomePage(){
 
   return(
   <div>
     <Header/>
-    <div class="container d-flex text-light">
-    <Content/>
-    <Stock/>
+    <div class="container text-light">
+      <div class="d-flex">
+        <Content/>
+        <Stock/>
+      </div>
+
+      <br></br>
+      <br></br>
+      <HomePageIntro/>
+      <br></br>
+      <br></br>
+      <HomePageFeature/>
+      <br></br>
+      <br></br>
+      <HomePageReviewIntro/>
+      <br></br>
+      <br></br>
+      <ReviewContent/>
 
 
 
     </div>
-    <Footer/>
-    </div>)
+      <br></br>
+      <br></br>
+      <Footer/>
+  </div>)
 }
