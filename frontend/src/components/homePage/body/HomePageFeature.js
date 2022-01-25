512efe8a314424f976e9245d169ca95a7d12d438
Yusuf Kortobi <kort8325@bears.unco.edu>
2022-01-25 16:06:36 -0700

    homepage edits

--- frontend/src/components/homePage/body/HomePageFeature.js
+++ frontend/src/components/homePage/body/HomePageFeature.js
@@ -6,11 +6,11 @@ export default function HomePageFeature(){
       <div class="row gx-5">
         <div class="col-4">
           <div class="p-3 card text-white bg-dark mb-3" >
-          <div class="card-header">Easy</div>
+          <div class="card-header">Header</div>
               <div class="card-body">
               
                 <h5 class="card-title">Easy</h5>
-                <p class="card-text">Unlike similar projects, ours was built with stock information in mind, so discussing and promoting the trends you have seen is easy.</p>
+                <p class="card-text">Unlike other stock discussion pages, ours was created with stock information in mind.  Making and sharing  </p>
               </div>
           </div>
         </div>
@@ -19,7 +19,7 @@ export default function HomePageFeature(){
           <div class="card-header">Header</div>
               <div class="card-body">
                 <h5 class="card-title">Simple</h5>
-                <p class="card-text">Want to make a post? All you have to do is create an account, select a stock you wish to use, and create your post!</p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
@@ -28,7 +28,7 @@ export default function HomePageFeature(){
             <div class="card-header">Header</div>
               <div class="card-body">
                 <h5 class="card-title">Free</h5>
-                <p class="card-text">There are no barriers to entry on this platform, all you have to do is find a post you'd like to start a discussion about and make a comment</p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
           </div>
         </div>
