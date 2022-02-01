e4257022ae4bb46b8256c31e8fc378087f3e49c8
Floydh22 <fnhaslett22@gmail.com>
2022-02-01 14:29:32 -0700

    aboutUpdate2/1
    Default bio and page layout
    linkedin links

--- frontend/src/components/aboutPage/About.js
+++ frontend/src/components/aboutPage/About.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Footer from "../homePage/footer/Footer";
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
 
 export default function About(){
   return(
@@ -11,12 +12,40 @@ export default function About(){
     [Insert Webisite Name Here] is the culmination of one whole school year dedicated to learning how to develop a project from start to finish. The goal of this webisite was to fill the social media void for stock trading. 
     Through the expressed need for a dedicated sight seen through the many reddit threds and facebook groups, [Insert Webisite Name Here] was born. 
   </p>
-
-
-
-
+  <p className = "text-light">
+    <b><u>Isaac Ingalls</u>: Project Lead</b><br/>
+    Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
+    using management skills and large scale ideas to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates 
+    in weightlighting and a competitive bowling league. <br/>
+    <a href = "https://www.linkedin.com/in/isaac-ingalls-5609a017b/" target="_blank">LinkedIn</a>  
+  </p>
+  <p className = "text-light" align = "right">
+  <b><u>Junwen Huang</u>: Frontend Developer</b><br/>
+    Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
+    using management skills and large scale ideas to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates 
+    in weightlighting and a competitive bowling league. <br/>
+    <a href = "https://www.linkedin.com/in/junwen-huang-658aaa202/" target="_blank">LinkedIn</a> 
+  </p>
+  <p className = "text-light" >
+    <b><u>Erik Halenkamp</u>: Backend Developer</b><br/>
+    Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
+    using management skills and large scale ideas to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates 
+    in weightlighting and a competitive bowling league. <br/>
+    <a href = "https://www.linkedin.com/in/erik-halenkamp-4b49451a6/" target="_blank">LinkedIn</a>  
+  </p>
+  <p className = "text-light" align = "right">
+  <b><u>Yusuf Kortobi</u>: Frontend Developer</b><br/>
+    Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
+    using management skills and large scale ideas to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates 
+    in weightlighting and a competitive bowling league. <br/><a href = "https://www.linkedin.com/in/yusuf-kortobi-aa496b19b/" target="_blank"> LinkedIn</a>  
+  </p>
+  <p className = "text-light" >
+  <b><u>Floyd Haslett</u>: System Security</b> <br/>
+    Floyd is a third year Business Computer Information Systems Major interested in Network and Securirty Systems Administration. He started this project with minimal knowledge in 
+    C# and worked with the team to learn aditional languages of HTTP, CSS, JavaScript, and React. In his free time he enjoys rock climbing, swimming, and is a network admin intern at
+    a local Greeley Business <br/>
+    <a href = "https://www.linkedin.com/in/floyd-haslett/" target="_blank"> LinkedIn</a>
+    <br/><br/><br/><br/><br/><br/><br/><br/>
+  </p>
   </div>)
-
-
-
 }
