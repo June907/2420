c513ac188960027b5f54aca3e68a64e283654a30
Yusuf Kortobi <kort8325@bears.unco.edu>
2022-02-01 16:34:21 -0700

    yusuf edits to about page

--- frontend/src/components/aboutPage/About.js
+++ frontend/src/components/aboutPage/About.js
@@ -10,18 +10,16 @@ export default function About(){
   <p className = "text-light">
     This website was created by the team named '2 for 20' as their senior project at the University of Northern Colorado. The team is made of five students from both the Business Admin: Computer Information Systems and the Software Engineering majors. 
     [Insert Webisite Name Here] is the culmination of one whole school year dedicated to learning how to develop a project from start to finish. The goal of this webisite was to fill the social media void for stock trading. 
-    Through the expressed need for a dedicated sight seen through the many reddit threds and facebook groups, [Insert Webisite Name Here] was born. <br/>
-  </p> 
-  <br></br>
+    Through the expressed need for a dedicated sight seen through the many reddit threds and facebook groups, [Insert Webisite Name Here] was born. 
+  </p>
+  <br/>
   <p className = "text-light">
-    <b><u>Isaac Ingalls</u>: Project Manager</b><br/>
-    Isaac is a fourth year Business Computer Information Systems Major interested in data and business analytics. Stocks and analyzing future investments are passion projects
-    that he had before this class, so he took the lead roll in this project. He uses the management skills and large scale ideas learned through the Monfort College of Business
-    to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates in weightlighting and a competitive bowling 
-    league. <br/>
+    <b><u>Isaac Ingalls</u>: Project Lead</b><br/>
+    Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
+    using management skills and large scale ideas to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates 
+    in weightlighting and a competitive bowling league. <br/>
     <a href = "https://www.linkedin.com/in/isaac-ingalls-5609a017b/" target="_blank">LinkedIn</a>  
   </p>
-  <br></br>
   <p className = "text-light" align = "right">
   <b><u>Junwen Huang</u>: Frontend Developer</b><br/>
     Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
@@ -29,7 +27,6 @@ export default function About(){
     in weightlighting and a competitive bowling league. <br/>
     <a href = "https://www.linkedin.com/in/junwen-huang-658aaa202/" target="_blank">LinkedIn</a> 
   </p>
-  <br></br>
   <p className = "text-light" >
     <b><u>Erik Halenkamp</u>: Backend Developer</b><br/>
     Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
@@ -37,14 +34,16 @@ export default function About(){
     in weightlighting and a competitive bowling league. <br/>
     <a href = "https://www.linkedin.com/in/erik-halenkamp-4b49451a6/" target="_blank">LinkedIn</a>  
   </p>
-  <br></br>
   <p className = "text-light" align = "right">
+    <div class="image">
+      <img src="../../static/admin/img/YK.jfif"></img>
+    </div>
   <b><u>Yusuf Kortobi</u>: Frontend Developer</b><br/>
-    Isaac is a third year Business Computer Information Systems Major interested in data analytics and large scale backend calculations. He took the lead roll in this project 
-    using management skills and large scale ideas to guide the more technical members of the team to create the website we planned and invisioned. Outside of school he participates 
-    in weightlighting and a competitive bowling league. <br/><a href = "https://www.linkedin.com/in/yusuf-kortobi-aa496b19b/" target="_blank"> LinkedIn</a>  
+    Yusuf is a 4th year student studying Software Engineering at UNC, graduating in May 2022. He took a frontend role, taking on many of the web design tasks 
+    while using many of the skills he learned while studying computer science. Professionally, Yusuf has worked with National STEM Honor Society as an web design intern, and as 
+    an RA in housing at UNC.  Outside of school he participates in competitve swim, as well as weightlifting and climbing for fun. <br/>
+    <a href = "https://www.linkedin.com/in/yusuf-kortobi-aa496b19b/" target="_blank"> LinkedIn</a>  
   </p>
-  <br></br>
   <p className = "text-light" >
   <b><u>Floyd Haslett</u>: System Security</b> <br/>
     Floyd is a third year Business Computer Information Systems Major interested in Network and Securirty Systems Administration. He started this project with minimal knowledge in 
