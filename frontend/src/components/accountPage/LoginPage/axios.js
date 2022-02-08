9b28fe64a16ae91cefe5cb40fecd16fe2cfeb477
June <98843235+JunRVW@users.noreply.github.com>
2022-02-08 16:02:20 -0700

    change back to pythonanywhere

--- frontend/src/components/accountPage/LoginPage/axios.js
+++ frontend/src/components/accountPage/LoginPage/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:"http://localhost:8000/api/users"
+  baseURL:"https://2for20.pythonanywhere.com/api/users"
+  //"http://localhost:8000/api/users"
+  //https://2for20.pythonanywhere.com/api/users/token
 })
