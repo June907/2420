038fd8cde9320e9dc15acbc2bc0032464f78a9ab
ewhalenkamp <70407222+ewhalenkamp@users.noreply.github.com>
2022-02-08 17:14:37 -0700

    attempt at api fix

--- frontend/src/components/accountPage/LoginPage/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL:"https://2for20.pythonanywhere.com/api/users"
-  //"http://localhost:8000/api/users"
-  //https://2for20.pythonanywhere.com/api/users/token
-})
