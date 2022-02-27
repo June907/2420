963b892b5011f76db69f2fdb8c105b7148a9a4fc
JunRVW <98843235+JunRVW@users.noreply.github.com>
2022-02-27 16:58:06 -0700

    user info

--- frontend/src/components/accountPage/ProfileHome.js
+++ frontend/src/components/accountPage/ProfileHome.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./FeedPage/Sidebar/Sidebar";
 import "./ProfileHome.css";
+import axios from "./LoginPage/axios";
 export default function ProfileHome(){
+  const base_url='https://twofortwenty-dev.us-west-2.elasticbeanstalk.com/api/users/current'
+  const user_url='/current'
+  const [users,setUser]=useState("")
+
+  axios.get(user_url)
+  .then(res=>{
+    console.log(res);
+    setUser(res.data.user.username);
+  })
+
 
   return(
 
-    <div className="container mainlayout">
+    <div className="container main-layout">
       <Sidebar/>
+      <h1>Hello {users}</h1>
+
 
     </div>
   )
