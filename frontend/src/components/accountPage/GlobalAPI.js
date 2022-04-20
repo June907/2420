14ef5d55a5f866b7366145f35b66b35eade47b5f
ewhalenkamp <70407222+ewhalenkamp@users.noreply.github.com>
2022-04-19 21:25:15 -0600

    seamless transition from logged in to out

--- frontend/src/components/accountPage/GlobalAPI.js
+++ frontend/src/components/accountPage/GlobalAPI.js
@@ -12,30 +12,108 @@ export default async function GlobalAPI(isGet, url, data) {
         })
             .then(res => {
                 console.log(res);
-                console.log('HI! ' + res.status);
-                (async () => {
-                    if (res.status === 401) {
-                        await axios.get(token_url)
-                            .then(respond => {
-                                // console.log('HEY! ' + respond.status)
-                                if (respond.status === 401) {
-                                    console.log('Need to login again.');
-                                    return res;
-                                };
-                            })
-                    }
-                })();
+                // (async () => {
+                //     if (res.status === 401) {
+                //         await axios.get(token_url)
+                //             .then(respond => {
+                //                 console.log('HEY! ' + respond.status)
+                //                 if (respond.status === 401) {
+                //                     console.log('Need to login again.');
+                //                     return res;
+                //                 };
+                //             })
+                //     }
+                // })();
                 r = res;
-                console.log(r);
                 return res;
-
             })
-            .catch(function (error) {
+            .catch(async function (error) {
                 if (error.response) {
                     // The request was made and the server responded with a status code
                     // that falls out of the range of 2xx
                     console.log("Path 1");
-                    console.log(error.response.data);
+                    if (401 === error.response.status) {
+                        const fetch = async () => {
+                            let re;
+                            await axios.post(url, data, {
+                                headers: { 'Content-Type': 'application/json' },
+                                withCredentials: true
+                            })
+                                .then(res => {
+                                    re = res;
+                                    console.log('HIYA');
+                                    return re;
+                                });
+                            console.log('ended');
+                            return re;
+                        }
+                        r = await fetch();
+                        console.log('Ho');
+                        console.log(r);
+                    }
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log("Path 2");
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+
+            });
+        console.log(url);
+        console.log('HEY');
+        console.log(r);
+        return r;
+    }
+    else {
+        await axios.get(url, {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        })
+            .then(res => {
+                console.log(res);
+                // console.log('HI! ' + res.status);
+                // (async () => {
+                //     if (res.status === 401) {
+                //         await axios.get(token_url)
+                //             .then(respond => {
+                //                 console.log('HEY! ' + respond.status)
+                //                 if (respond.status === 401) {
+                //                     console.log('Need to login again.');
+                //                     return res;
+                //                 };
+                //             })
+                //     }
+                // })();
+                r = res;
+                return res;
+            })
+            .catch(async function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("Path 1");
+                    if (401 === error.response.status) {
+                        const fetch = async () => {
+                            let re;
+                            await axios.get(url, {
+                                headers: { 'Content-Type': 'application/json' },
+                                withCredentials: true
+                            })
+                                .then(res => {
+                                    re = res;
+                                    return re;
+                                });
+                            console.log('ended');
+                            return re;
+                        }
+                        r = await fetch();
+                        console.log('Ho');
+                        console.log(r);
+                    }
                 } else if (error.request) {
                     // The request was made but no response was received
                     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
@@ -49,32 +127,8 @@ export default async function GlobalAPI(isGet, url, data) {
                 console.log(error.config);
             });
         console.log(url);
-        return r;
-    }
-    else {
-        await axios.get(url, {
-            headers: { 'Content-Type': 'application/json' },
-            withCredentials: true
-        })
-            .then(res => {
-                console.log(res);
-                console.log('HI! ' + res.status);
-                (async () => {
-                    if (res.status === 401) {
-                        await axios.get(token_url)
-                            .then(respond => {
-                                console.log('HEY! ' + respond.status)
-                                if (respond.status === 401) {
-                                    console.log('Need to login again.');
-                                    return res;
-                                };
-                            })
-                    }
-                })();
-                r = res;
-                return res;
-            })
-        console.log(url);
+        console.log('HEY');
+        console.log(r);
         return r;
     }
 }
