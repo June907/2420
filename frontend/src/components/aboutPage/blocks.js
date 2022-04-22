53139ca11b85f6357897177d654b433197341736
JunRVW <98843235+JunRVW@users.noreply.github.com>
2022-04-22 10:59:29 -0600

    bio update

--- frontend/src/components/aboutPage/blocks.js
+++ frontend/src/components/aboutPage/blocks.js
@@ -32,9 +32,7 @@ function Blocks() {
         <div className="blocks-text">
             <p className = "text-light">
                 <b><u>Junwen Huang</u>: Frontend Developer</b><br/>
-                Junwen is a fouth year studnet studying both Software Engineering and Applied Mathematics at UNC, graduating in Dec 2022. As the only person that knows React frame work,
-                He took a frontend role, implenting React framework as well as intergrating external API into the websiteProfessionally, Junwen is working as an Intern in RedVan Workshop as SFCC developr. 
-                In his free time, he like play Souls series games.
+                Junwen is a fourth-year student studying software engineering and applied mathematics, who will be graduating in December 2022. Junwen had the most experience with the react framework, so he took the lead frontend role. He used this knowledge to develop the frontend and integrate it with an external API. Professionally, Junwen is working as an intern at RedVan workshop with his primary role being SFCC development.
                 <br/>
                 <a href = "https://www.linkedin.com/in/junwen-huang-658aaa202/" target="_blank" rel="noreferrer">LinkedIn</a>
             </p> 
